0f1760b855376bcaaf364d0e4e1ff2ec352987fa
Md Hyder <mdhyder@buffalo.edu>
2023-12-28 17:32:36 -0500

    Refactor data processing

--- src/data_processing/data_processing.py
+++ src/data_processing/data_processing.py
@@ -1,26 +1,51 @@
-import pandas as pd
+import csv
+from collections import Counter
+from datetime import datetime
 
-def process_data(data):
-    # Remove rows with any missing values
-    data = data.dropna()
-    
-    # Convert the 'Date' column to datetime type
-    data['Date'] = pd.to_datetime(data['Date'])
-    
-    # Set the 'Date' column as the index of the DataFrame
-    data.set_index('Date', inplace=True)
-    
-    # Sort data by date (from oldest to newest)
-    data.sort_index(inplace=True)
-    
-    # Calculate daily returns
-    data['Daily Return'] = data['Close'].pct_change()
-    
-    # Calculate moving averages
-    data['30 Day Avg'] = data['Close'].rolling(window=30).mean()
-    data['100 Day Avg'] = data['Close'].rolling(window=100).mean()
-    
-    # Dropping all 'NaN' values
-    data.dropna(inplace=True)
-    
-    return data
+def read_csv(filename):
+    """Reads a CSV file and returns a list of dictionaries for its contents."""
+    with open(filename, newline='', encoding="utf-8") as f:
+        return list(csv.DictReader(f))
+
+def write_csv(filename, list_of_dictionaries):
+    """Writes a list of dictionaries to a CSV file."""
+    with open(filename, "w", newline='', encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldnames=list_of_dictionaries[0].keys())
+        writer.writeheader()
+        writer.writerows(list_of_dictionaries)
+
+def filter_by_key(list_of_dictionaries, key, value):
+    """Returns a list of dictionaries that contain the given key-value pairing."""
+    return [entry for entry in list_of_dictionaries if entry[key] == value]
+
+def discard_by_key(list_of_dictionaries, key, value):
+    """Returns a list of dictionaries that do not contain the given key-value pairing."""
+    return [entry for entry in list_of_dictionaries if entry[key] != value]
+
+def filter_range(list_of_dictionaries, key, low, high):
+    """Returns a list of dictionaries within a specified range of a given key."""
+    return [entry for entry in list_of_dictionaries if low <= entry[key] < high]
+
+def calculate_duration(date1_str, date2_str):
+    """Calculates the duration in days between two dates."""
+    date_format = "%m/%d/%Y"  # Assuming the date format is like '01/31/2020'
+    date1 = datetime.strptime(date1_str, date_format)
+    date2 = datetime.strptime(date2_str, date_format)
+    return (date2 - date1).days
+
+def get_departments(list_of_dictionaries):
+    """Returns a sorted list of unique departments."""
+    return sorted(set(entry["SUBJECT"] for entry in list_of_dictionaries))
+
+def get_open_year(entry):
+    """Returns the year a request was opened."""
+    return entry["OPEN DATE"].split('/')[-1]
+
+def filter_year(list_of_dictionaries, low, high):
+    """Filters entries based on year range."""
+    return [entry for entry in list_of_dictionaries if low <= get_open_year(entry) < high]
+
+def parse_date(date_str):
+    """Converts a date string to a datetime.date object."""
+    return datetime.strptime(date_str, "%m/%d/%Y").date()
+
